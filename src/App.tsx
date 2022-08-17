0743f68d0863419fa702a3c02f95f7772d0764dc
JHuaWei <2237268069@qq.com>
2022-08-17 23:02:33 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,97 @@
-import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import React from "react";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height:100vh ;
+  display: flex;
+  flex-direction: column;
+  
+`
+
+const Main = styled.div`
+  border: 1px solid green;
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  >ul {
+    display: flex;
+    >li{
+      width: 33.3%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`
 
 
 function App() {
   return (
-    <div>
-      你好
+    <Router>
+      <Wrapper>
+        <Main>
+          <Routes>
+            <Route path="/tags" element={<Tags />}>
 
-    </div>
+            </Route>
+            <Route path="/money" element={<Money />}>
+
+            </Route>
+            <Route path="/statistics" element={<Statistics />}>
+
+            </Route>
+            <Route path="/" element={<Money />}>
+
+            </Route>
+            <Route path="*" element={<NoMatch />}>
+
+            </Route>
+
+          </Routes>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+
+      </Wrapper>
+    </Router>
   );
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+function NoMatch() {
+  return <h2>NoMatch 404</h2>
 }
 
 export default App;
